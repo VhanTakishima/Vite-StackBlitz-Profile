dea5296e5cf9d9d0b0d22c7dcca437888fe1e77e
VhanTakishima <144688177+VhanTakishima@users.noreply.github.com>
2023-09-15 22:39:09 +0800

    1 file modified

--- textanim.js
+++ textanim.js
@@ -1,5 +1,5 @@
 let typed = new Typed(".typing",{
-  strings:["Web Developer","Web Designer","Graphic Artist","Graphic Designer","Presentation Expert","Gamer","Game Mods Hobbist"],
+  strings:["Web Developer","Web Designer","Graphic Artist","Graphic Designer","Novice Animator","Presentation Expert","Gamer","Game Mods Hobbist"],
   typeSpeed:100,
   Backspeed:60,
   loop:true,
