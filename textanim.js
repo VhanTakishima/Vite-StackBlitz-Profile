a8add0e7012ffdaabba84c29591346986876dbfb
VhanTakishima <144688177+VhanTakishima@users.noreply.github.com>
2023-09-15 22:29:49 +0800

    1 binaries uploaded

---
+++ textanim.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+  strings:["Web Developer","Web Designer","Graphic Artist","Graphic Designer","Presentation Expert","Gamer","Game Mods Hobbist"],
+  typeSpeed:100,
+  Backspeed:60,
+  loop:true,
+})
