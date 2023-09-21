e5d849049333d034be954b3b6e59ae37c5ac588c
VhanTakishima <144688177+VhanTakishima@users.noreply.github.com>
2023-09-21 21:30:16 +0800

    3 files modified, 1 added

--- textanim.js
+++ textanim.js
@@ -5,31 +5,31 @@ var typed = new Typed(".typing",{
   loop:true,
 })
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
+// let interval = null;
 
-document.querySelector(".randomletter").onmouseover = event => {  
-  let iteration = 0;
+// document.querySelector(".randomletter").onmouseover = event => {  
+//   let iteration = 0;
   
-  clearInterval(interval);
+//   clearInterval(interval);
   
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
+//   interval = setInterval(() => {
+//     event.target.innerText = event.target.innerText
+//       .split("")
+//       .map((letter, index) => {
+//         if(index < iteration) {
+//           return event.target.dataset.value[index];
+//         }
       
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
+//         return letters[Math.floor(Math.random() * 26)]
+//       })
+//       .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
-    }
+//     if(iteration >= event.target.dataset.value.length){ 
+//       clearInterval(interval);
+//     }
     
-    iteration += 1 / 3;
-  }, 30);
-}
+//     iteration += 1 / 3;
+//   }, 30);
+// }
