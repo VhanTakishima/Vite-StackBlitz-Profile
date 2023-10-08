fa674dc0749136020c1afa463086d35d335485d0
VhanTakishima <144688177+VhanTakishima@users.noreply.github.com>
2023-10-08 13:45:01 +0800

    text

--- textanim.js
+++ textanim.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-  strings:["Web Developer","Web Designer","Graphic Artist","Graphic Designer","Novice Animator","Presentation Expert","Gamer","Game Mods Hobbist"],
+  strings:["Web Developer","Web Designer","Graphic Artist","Graphic Designer","Novice Animator","Presentation Expert","Gamer"],
   typeSpeed:100,
   Backspeed:100,
   loop:true,
