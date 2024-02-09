a16ae0553a5b2a962414c43c73b57075b2e0685c
Vhan Takishima <vhantakishima@gmail.com>
2024-02-09 16:24:31 +0800

    Chatbot 2 b continue

---
+++ JS/chatBot.js
@@ -0,0 +1,13 @@
+let cToggler = document.querySelector(".chatToggler");
+let cSwitch = document.querySelector("#chatSwitch");
+
+
+function chatToggle(){
+if (cSwitch.classList.contains("show-chatbox")) {
+cSwitch.classList.remove("show-chatbox");
+}
+else{   
+    cSwitch.classList.add("show-chatbox");   
+}
+
+}
